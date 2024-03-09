4882e2dbdbfd137835f7baa96a12f59f97e72a8e
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-03-10 00:25:35 +0900

    :lipstick:  Fix: PWA App Icon 수정 / 앱 이름, start_url 수정

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,8 +3,18 @@ import { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
   return {
     theme_color: "#ffffff",
-    background_color: "#ffffff",
+    background_color: "#f5f5f5",
     icons: [
+      {
+        src: "app-icon/ios/192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "app-icon/ios/512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
       {
         src: "app-icon/ios/192.png",
         sizes: "192x192",
