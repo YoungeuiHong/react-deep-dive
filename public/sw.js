bd825f43d4ec5a064969d8b9d0ec2e937a4cd004
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-03-13 21:28:08 +0900

    :art: Refactor: sw.js 코드 리팩토링

--- public/sw.js
+++ public/sw.js
@@ -1,46 +1,15 @@
-const cacheName = "v1";
-
-// const urlsToCache = ["/next.svg", "/pwa-todo"];
-// self.addEventListener("install", (event) => {
-//   console.log("service worker installed");
-//   event.waitUntil(
-//     caches.open("pwa-assets").then((cache) => {
-//       return cache.addAll(urlsToCache);
-//     }),
-//   );
-// });
-
 self.addEventListener("activate", () => {
   console.log("service worker activated");
 });
 
-// const cacheClone = async (e) => {
-//   const res = await fetch(e.request);
-//   const resClone = res.clone();
-//
-//   const cache = await caches.open(cacheName);
-//   await cache.put(e.request, resClone);
-//   return res;
-// };
-//
-// /**
-//  * ServiceWorkerGlobalScope: fetch event
-//  * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerGlobalScope/fetch_event)
-//  * The fetch event is fired in the service worker's global scope when the main app thread makes a network request.
-//  * It enables the service worker to intercept network requests and send customized responses (for example, from a local cache).
-//  */
-// const fetchEvent = () => {
-//   self.addEventListener("fetch", (e) => {
-//     console.log("💚 fetchEvent 발생!");
-//     e.respondWith(
-//       cacheClone(e)
-//         .catch(() => caches.match(e.request))
-//         .then((res) => res),
-//     );
-//   });
-// };
-//
-// fetchEvent();
+self.addEventListener("push", function (event) {
+  const { title, message } = event.data.json();
+  const options = {
+    body: message,
+    icon: "/app-icon/ios/192.png",
+  };
+  event.waitUntil(self.registration.showNotification(title, options));
+});
 
 function periodicCheck() {
   const request = indexedDB.open("pwa-db");
@@ -102,12 +71,3 @@ function periodicCheck() {
 
 // 1분마다 알려야 할 알림이 있는지 체크
 setInterval(periodicCheck, 60 * 1000);
-
-self.addEventListener("push", function (event) {
-  const { title, message } = event.data.json();
-  const options = {
-    body: message,
-    icon: "/app-icon/ios/192.png",
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
-});
