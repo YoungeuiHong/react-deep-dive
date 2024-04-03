74ea224e7de8050d714073343781aae91508c2c3
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-04 03:29:15 +0900

    :memo: chore: swiper-and-flip auto transition 코드 백업

--- src/app/swiper-and-flip/page.tsx
+++ src/app/swiper-and-flip/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import type { MouseEvent } from "react";
 import { useEffect, useRef, useState } from "react";
 
 type SwipeStatus =
@@ -18,18 +17,16 @@ const GestureRecognizer = () => {
   const [cardIndex, setCardIndex] = useState(0);
   const [description, setDescription] =
     useState<SwipeStatus>("AUTO TRANSITION");
-  const [startX, setStartX] = useState<number>(0);
-  const [dragging, setDragging] = useState<boolean>(false);
-  const [width, setWidth] = useState<number>(400);
-  const [transitionOffset, setTransitionOffset] = useState(0);
-  const [transitionOpacity, setTransitionOpacity] = useState(1);
-  const [animation, setAnimation] = useState<Animation | null>(null);
-  const [intervalId, setIntervalId] = useState<number | null>(null);
 
   useEffect(() => {
     if (!animationRef.current) {
       return;
     }
+
+    // const keyframes: Keyframe[] | PropertyIndexedKeyframes = [
+    //   { opacity: 1, transform: "translateX(0%)", offset: 0 },
+    //   { opacity: 0, transform: "translateX(100%)", offset: 1 },
+    // ];
 
     const keyFrameEffect = new KeyframeEffect(
       animationRef.current,
@@ -46,111 +43,27 @@ const GestureRecognizer = () => {
       },
     );
 
-    const animation = new Animation(keyFrameEffect);
-
-    setAnimation(animation);
+    let animation: Animation = new Animation(keyFrameEffect);
 
     function playAnimation() {
-      if (!animation) return;
       animation.play();
     }
 
     function changeCard() {
-      playAnimation();
       setCardIndex((prev) => (prev + 1) % 4);
+      playAnimation();
     }
 
     playAnimation();
-    const id = window.setInterval(changeCard, 3000);
-    setIntervalId(id);
+
+    const intervalId = window.setInterval(changeCard, 3000);
 
     return () => {
       if (intervalId) {
         clearInterval(intervalId);
       }
-      if (animation) {
-        animation.cancel();
-      }
     };
   }, []);
-
-  // useEffect(() => {
-  //   if (dragging) {
-  //     if (intervalId) {
-  //       clearInterval(intervalId);
-  //     }
-  //     if (animation) {
-  //       animation.pause();
-  //     }
-  //   }
-  // }, [dragging]);
-
-  function handleMouseDown(event: MouseEvent) {
-    event.preventDefault();
-    setDragging(true);
-    setStartX(event.clientX);
-    setWidth(event.currentTarget.clientWidth);
-    setTranslateX(0);
-  }
-
-  function handleMouseMove(event: MouseEvent) {
-    event.preventDefault();
-
-    if (!dragging) {
-      return;
-    }
-
-    const currentX = event.clientX;
-    const deltaX = currentX - startX;
-    setDescription(deltaX < 0 ? "SWIPE left" : "SWIPE right");
-    const offset = (deltaX * 100) / width;
-    setTranslateX(deltaX);
-    setTransitionOffset(offset);
-    if (animationRef.current) {
-      animationRef.current.style.transform = `translateX(${transitionOffset}%)`;
-      animationRef.current.style.opacity = `${1 - Math.abs(deltaX) / width}`;
-      animationRef.current.style.transition = "all 0.3s ease-in-out";
-    }
-  }
-
-  function performTransition() {
-    setTransitionOffset(100);
-    setTransitionOpacity(0);
-    setTimeout(() => {
-      setCardIndex((prev) => (prev + 1) % 4);
-      setTransitionOffset(0);
-    }, 300);
-  }
-
-  function handleMouseUp(event: MouseEvent) {
-    event.preventDefault();
-    setDragging(false);
-    const deltaX = translateX - startX;
-    if (Math.abs(deltaX) / width >= 0.5) {
-      if (deltaX < 0) {
-        setDescription("TO LEFT");
-      } else {
-        setDescription("TO RIGHT");
-      }
-      // performTransition();
-      setCardIndex((prev) => (prev + 1) % 4);
-    } else {
-      setDescription("CANCEL");
-    }
-    setTranslateX(0);
-    setTransitionOffset(0);
-    setTransitionOpacity(1);
-  }
-
-  const handleMouseLeave = (event: MouseEvent) => {
-    event.preventDefault();
-    if (!dragging) return;
-
-    // setDragging(false);
-    setTranslateX(0);
-    // setTransitionOffset(0);
-    // setTransitionOpacity(1);
-  };
 
   return (
     <div
@@ -177,7 +90,7 @@ const GestureRecognizer = () => {
             backgroundColor: `${arr[(cardIndex + 1) % 4]}`,
             textAlign: "center",
             alignItems: "center",
-            fontWeight: 900,
+            fontWeight: "bold",
             color: "white",
             borderRadius: "4px",
             touchAction: "none",
@@ -200,12 +113,11 @@ const GestureRecognizer = () => {
             height: "100%",
             backgroundColor: `${arr[cardIndex]}`,
             textAlign: "center",
-            fontWeight: 900,
+            fontWeight: "bold",
             color: "white",
             alignItems: "center",
             borderRadius: "4px",
             touchAction: "none",
-            transition: dragging ? "all 0.3s ease-in-out" : undefined,
           }}
         >
           <div
@@ -223,12 +135,8 @@ const GestureRecognizer = () => {
             left: translateX,
             width: "100%",
             height: "100%",
-            cursor: dragging ? "grabbing" : "grab",
+            cursor: "pointer",
           }}
-          onMouseDown={handleMouseDown}
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-          onMouseLeave={handleMouseLeave}
         />
       </div>
     </div>
