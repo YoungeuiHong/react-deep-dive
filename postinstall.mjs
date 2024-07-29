5f176d271a403fd766ef5b618ed5f109cfb7ef64
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-07-29 14:08:50 +0900

    :test_tube: Chore: TinyMCE Self-hosted Installation 테스트

---
+++ postinstall.mjs
@@ -0,0 +1,14 @@
+import fse from "fs-extra";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const topDir = __dirname;
+fse.emptyDirSync(path.join(topDir, "public", "tinymce"));
+fse.copySync(
+  path.join(topDir, "node_modules", "tinymce"),
+  path.join(topDir, "public", "tinymce"),
+  { overwrite: true },
+);
